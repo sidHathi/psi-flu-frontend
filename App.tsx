cd0385aa6f21a7e205c5df7255b71ad873d8fbab
brodythompson1 <117129129+brodythompson1@users.noreply.github.com>
2023-04-16 05:58:35 -0400

    added Home Banner

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { CommonSymptoms } from './components/CommonSymptoms/CommonSymptoms';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthContainer from './components/Auth/AuthContainer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -10,7 +10,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 import { AppBar } from './components/AppBar/AppBar';
 import { GraphComp } from './components/GraphComp/GraphComp';
 import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
-
+import GlobalStyles from './components/GlobalStyles';
 type RootStackParamList = {
   Login: undefined;
   Profile: undefined;
@@ -22,27 +22,31 @@ export default function App() {
 
   const Stack = createNativeStackNavigator();
 
- 
+
   const HomeScreen = ({ navigation }) => {
-   
+
     return (
       <SafeAreaProvider>
         <SafeAreaView style={styles.container}>
-          <GraphComp open={true}/>
+
+          <Text style={styles.Text}>Home</Text>
+
+
+          <GraphComp open={true} />
           <CommonSymptoms open={true} />
-          <AppBar open={true} navigation={navigation}/>
+          <AppBar open={true} navigation={navigation} />
         </SafeAreaView>
       </SafeAreaProvider>
     );
   };
-  const ProfileScreen = ({navigation }) => {
-  
+  const ProfileScreen = ({ navigation }) => {
+
     return (
       <SafeAreaProvider>
         <SafeAreaView style={styles.container}>
           <Text>This is my's profile</Text>
-          <AppBar open={true} navigation={navigation}/>
-          
+          <AppBar open={true} navigation={navigation} />
+
         </SafeAreaView>
       </SafeAreaProvider>
 
@@ -58,26 +62,26 @@ export default function App() {
             gestureDirection: 'horizontal',
             animationTypeForReplace: 'pop',
             fullScreenGestureEnabled: true,
-          
+
           }}
         >
 
-            <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
-            <Stack.Screen
-              name="Profile"
-              component={ProfilePage}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen 
-              name="Login" 
-              component={AuthContainer} 
-              options={{headerShown: false}}
-            />
+          <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+          <Stack.Screen
+            name="Profile"
+            component={ProfilePage}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={AuthContainer}
+            options={{ headerShown: false }}
+          />
 
         </Stack.Navigator>
-        
+
       </NavigationContainer>
-      
+
     </SafeAreaProvider>
   );
 }
@@ -89,5 +93,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
+  Text: {
+    ...GlobalStyles.Text,
+    color: '#000000',
+    fontSize: 38,
+    paddingTop: 40,
+    paddingLeft: 30,
+    alignSelf: 'flex-start'
+  },
+  HeaderBox: {
+
+    bottom: 0,
+    alignItems: 'flex-start',
+    height: 60
+  }
 });
 
